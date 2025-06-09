916df4ff0381fc58d4b2ca90fe40a49c0847c196
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-06-09 08:50:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        return '366';
+    } else {
+        return '365';
+    }
+}
+
+// take input
+const year = prompt('Enter a year:');
+
+alert(checkLeapYear(year));
